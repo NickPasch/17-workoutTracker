066ef46c60d2aea48e3cd15d9cbbbffee2c2ea42
Nick Pasch <paschnick100@gmail.com>
2021-01-21 19:31:12 -0700

    fully functioning deployment

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.use(express.static("public"));
 // end of this url?
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 // routes
